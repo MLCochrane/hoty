9b056c5f22724c6efb1a4502c264e4d8c46697da
Luke <lukecochrane2@gmail.com>
2019-07-03 20:18:20 -0400

    Typo correction

--- hoty-client/src/view/components/forms/FormUserDetails.js
+++ hoty-client/src/view/components/forms/FormUserDetails.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
-// import Paper from '@material-ui/core/Paper';
-import FromLogin from './FormLogin';
+import FormLogin from './FormLogin';
 import FormRegister from './FormRegister';
 
 
 class FormUserDetails extends Component {
 	render() {
 		return (this.props.form === 'login'
-			? <FromLogin />
+			? <FormLogin />
 			: <FormRegister />
 		);
 	}
