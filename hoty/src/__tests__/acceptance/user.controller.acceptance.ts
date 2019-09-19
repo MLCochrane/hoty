cda29d3587fed2a53650aad85fd5a9e8c2eaa30a
Luke <lukecochrane2@gmail.com>
2019-09-19 13:06:36 -0400

    Updating user controller acceptance tests with first /users POST tests

--- hoty/src/__tests__/acceptance/user.controller.acceptance.ts
+++ hoty/src/__tests__/acceptance/user.controller.acceptance.ts
@@ -28,11 +28,10 @@ describe('UserController', () => {
   const userRepo = new UserRepository(dbDS, eventRepo);
 
   const user = {
-    id: uuidv4() as string,
-    username: 'Username',
+    username: 'AwesomePal',
     password: 'p4ssw0rd',
     email: 'test@test.com',
-    firstName: 'Example',
+    firstName: 'Test',
     lastName: 'User',
   };
 
@@ -46,12 +45,115 @@ describe('UserController', () => {
   before(createPasswordHasher);
   before(givenAnExpiredToken);
 
-  beforeEach(clearDatabase);
-  // assertions
-
   after(async () => {
     await app.stop();
   });
+
+  describe('creating users', () => {
+    beforeEach(clearDatabase);
+
+    it('creates new user when POST /api/users is invoked', async () => {
+      const res = await client
+      .post('/api/users')
+      .send(user)
+      .expect(200);
+
+      expect(res.body.email).to.equal('test@test.com');
+      expect(res.body.firstName).to.equal('Test');
+      expect(res.body.lastName).to.equal('User');
+      expect(res.body.username).to.equal('AwesomePal');
+      expect(res.body).to.have.property('id');
+      expect(res.body).to.not.have.property('password');
+    });
+
+    it('throws error for POST /api/users with a missing email', async () => {
+      const res = await client
+      .post('/api/users')
+      .send({
+        username: 'username',
+        password: 'p4ssw0rd',
+        firstName: 'Example',
+        lastName: 'User',
+      })
+      .expect(422);
+
+      const errorText = JSON.parse(res.error.text);
+      expect(errorText.error.details[0].info.missingProperty).to.equal('email');
+    });
+
+    it('throws error for POST /api/users with a missing username', async () => {
+      const res = await client
+      .post('/api/users')
+      .send({
+        email: 'test@test.com',
+        password: 'p4ssw0rd',
+        firstName: 'Example',
+        lastName: 'User',
+      })
+      .expect(422);
+
+      const errorText = JSON.parse(res.error.text);
+      expect(errorText.error.details[0].info.missingProperty).to.equal('username');
+    });
+
+    it('throws error for POST /api/users with a missing password', async () => {
+      const res = await client
+      .post('/api/users')
+      .send({
+        email: 'test@test2.com',
+        username: 'John',
+      })
+      .expect(422);
+
+      const errorText = JSON.parse(res.error.text);
+      expect(errorText.error.details[0].info.missingProperty).to.equal('password');
+    });
+
+    it('throws error for POST /api/users with an existing email', async () => {
+      const userOne = givenUser({
+        email: 'testing@test.com',
+        username: 'newestUser'
+      });
+      const userTwo = givenUser({
+        email: 'testing@test.com',
+        username: 'evenNewerUser'
+      });
+
+      await client
+        .post('/api/users')
+        .send(userOne)
+        .expect(200);
+      const res = await client
+        .post('/api/users')
+        .send(userTwo)
+        .expect(409);
+
+      expect(res.body.error.message).to.equal('Email already in use');
+    });
+
+    it('throws error for POST /api/users with an existing username', async () => {
+      const userOne = givenUser({
+        email: 'test@tester.com',
+        username: 'popularUsername'
+      });
+      const userTwo = givenUser({
+        email: 'testing-unique@test.com',
+        username: 'popularUsername'
+      });
+
+      await client
+        .post('/api/users')
+        .send(userOne)
+        .expect(200);
+      const res = await client
+        .post('/api/users')
+        .send(userTwo)
+        .expect(409);
+
+      expect(res.body.error.message).to.equal('Username already in use');
+    });
+  });
+
 
   async function clearDatabase() {
     await userRepo.deleteAll();
@@ -63,8 +165,10 @@ describe('UserController', () => {
 
   async function createAUser() {
     const encryptedPassword = await passwordHasher.hashPassword(user.password);
-    user.password = encryptedPassword;
-    const newUser = await userRepo.create(user);
+    const leUser = givenUser({
+      password: encryptedPassword
+    });
+    const newUser = await userRepo.create(leUser);
     // MongoDB returns an id object we need to convert to string
     // newUser.id = newUser.id.toString();
 
@@ -92,6 +196,21 @@ describe('UserController', () => {
       name: `${newUser.firstName} ${newUser.lastName}`,
     };
     expiredToken = await tokenService.generateToken(userProfile);
+  }
+
+  function givenUser(user?: Partial<User>) {
+    const data = Object.assign(
+      {
+        id: uuidv4(),
+        username: 'Username',
+        password: 'p4ssw0rd',
+        email: 'test@test.com',
+        firstName: 'Example',
+        lastName: 'User',
+      },
+      user,
+    );
+    return new User(data);
   }
 });
 
